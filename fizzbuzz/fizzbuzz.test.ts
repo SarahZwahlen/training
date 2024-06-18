c990894374842b330a8f17992b0d4d664237bcea
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2024-06-18 19:21:32 +0200

    fizzbuzz step 2

--- fizzbuzz/fizzbuzz.test.ts
+++ fizzbuzz/fizzbuzz.test.ts
@@ -1,22 +1,29 @@
 import { fizzBuzz } from './fizzbuzz';
 
 describe('fizzbuzz', () => {
-    test('happy path', () => {
-        const allResult = () => {
-            const result = [];
-            for (let i = 1; i <= 100; i++) {
-                result.push(fizzBuzz(i));
-            }
-            console.log('result', result);
-        };
-
-        allResult();
-
+    test('Is dividable by 3', () => {
         expect(fizzBuzz(3)).toBe('fizz');
+    });
+
+    test('Is dividable by 5', () => {
         expect(fizzBuzz(5)).toBe('buzz');
-        expect(fizzBuzz(15)).toBe('fizzbuzz');
+    });
+
+    test('Return the value when conditions are not completed', () => {
         expect(fizzBuzz(8)).toBe(8);
+    });
+
+    test('Number contains a 3', () => {
         expect(fizzBuzz(32)).toBe('fizz');
+    });
+
+    test('Number contains a 5', () => {
         expect(fizzBuzz(52)).toBe('buzz');
+    });
+
+    test('Number contains some 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('fizzbuzz');
+        expect(fizzBuzz(53)).toBe('buzzfizz');
+        expect(fizzBuzz(353)).toBe('fizzbuzzfizz');
     });
 });
--- fizzbuzz/fizzbuzz.ts
+++ fizzbuzz/fizzbuzz.ts
@@ -1,23 +1,40 @@
-enum FIZZBUZZ {
-    fizz = 'fizz',
-    buzz = 'buzz'
-}
+type FIZZBUZZ = 'fizz' | 'buzz' | '';
 
 export const fizzBuzz = (number: number) => {
-    const testFizz = compute(number, 3);
-    const testBuzz = compute(number, 5);
+    const testModuloFizz = testModulo(number, 3);
+    const testModuloBuzz = testModulo(number, 5);
+    const testContainerNumber = testContainTargetNumber(number);
 
-    if (testFizz || testBuzz) {
-        return testFizz + testBuzz;
+    if (testModuloFizz || testModuloBuzz) {
+        return testModuloFizz + testModuloBuzz;
+    } else if (testContainerNumber) {
+        return testContainerNumber;
     } else {
         return number;
     }
 };
 
-const compute = (number: number, target: 3 | 5): string => {
-    const text = number.toString().split('');
-    if (number % target === 0 || text.includes(target.toString())) {
+const testModulo = (number: number, target: number): FIZZBUZZ => {
+    if (number % target === 0) {
         return target === 3 ? 'fizz' : 'buzz';
     }
     return '';
+};
+
+const testContainTargetNumber = (number: number): string => {
+    return number
+        .toString()
+        .split('')
+        .map((character) => {
+            if (character === '3') {
+                return 'fizz';
+            }
+
+            if (character === '5') {
+                return 'buzz';
+            }
+
+            return '';
+        })
+        .join('');
 };
